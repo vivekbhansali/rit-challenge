ad9f323967a9e68a9b01c79f442d226efb4f56a4
Vivek Bhansali <vivek_bhansali@live.com>
2016-03-06 02:28:12 -0500

    Update filtering process for categorization.
    Added criteria's and checks to get categorization for all transaction logs. Categorizing food transaction by matching it with pattern - {name} {number} and other similar updates for other categories.

--- Count.java
+++ Count.java
@@ -21,6 +21,7 @@ public class Count {
 	Counter entertainment = new Counter();
 	Counter utilities = new Counter();
 	Counter misc = new Counter();
+	Counter none = new Counter();
 	
 	public void iterate(List<List<String>> data) {
 		
@@ -32,55 +33,75 @@ public class Count {
 	public void checkCounts(String description) {
 		// TODO: Add more keywords to categorize all transactions
 		
-		//food
-		if ( description.toLowerCase().contains("pizza") || description.toLowerCase().contains("italian") ||
-				description.toLowerCase().contains("cafe") || description.toLowerCase().contains("market") ) {
-			//System.out.println(description);
+		//food - Pattern: '{name} {number}'
+		/*if ( description.toLowerCase().contains("pizza") || description.toLowerCase().contains("italian") ||
+				description.toLowerCase().contains("cafe") || description.toLowerCase().contains("market") )*/ 
+		if(!checkUpperCase(description) && description.matches("^.*?\\d$")){
+			System.out.println("Category:Food:: " + description);
 			food.increment();
-		}
+		} else {
 				
-		//travel
-		if ( description.toLowerCase().contains("uber") || description.toLowerCase().contains("gas") ||
-				description.toLowerCase().contains("shell") || description.toLowerCase().contains("flight") ) {
-			//System.out.println(description);
-			travel.increment();
-		}
-		
-		//shopping
-		if ( description.toLowerCase().contains("walmart") || description.toLowerCase().contains("target") ||
-				description.toLowerCase().contains("nordstrom") || description.toLowerCase().contains("amazon") ) {
-			//System.out.println(description);
-			shopping.increment();
-		}
-		
-		//entertainment
-		if ( description.toLowerCase().contains("netflix") || description.toLowerCase().contains("cinema") ||
-				description.toLowerCase().contains("movies") || description.toLowerCase().contains("theatre") ) {
-			//System.out.println(description);
-			entertainment.increment();
-		}
-		
-		//utilities
-		if ( description.toLowerCase().contains("rent") || description.toLowerCase().contains("internet") ||
-				description.toLowerCase().contains("firstservice") || description.toLowerCase().contains("electric") ) {
-			//System.out.println(description);
-			utilities.increment();
-		}
-		
-		//misc
-		if ( description.toLowerCase().contains("atm") || description.toLowerCase().contains("paypal") ) {
-			//System.out.println(description);
-			misc.increment();
+			//travel - MOBILE GAS, CHEVRON, UBER, SHELL
+			if ( description.toLowerCase().contains("uber") || description.toLowerCase().contains("gas") ||
+					description.toLowerCase().contains("shell") || description.toLowerCase().contains("chevron") ) {
+				System.out.println("Category:Travel:: " + description);
+				travel.increment();
+			} else {
+			
+				//shopping
+				if ( description.toLowerCase().contains("walmart") || description.toLowerCase().contains("target") ||
+						description.toLowerCase().contains("nordstrom") || description.toLowerCase().contains("amazon") ) {
+					System.out.println("Category:Shopping:: " + description);
+					shopping.increment();
+				} else {
+				
+					//entertainment - ADY*Netflix
+					if ( description.toLowerCase().contains("netflix") || description.toLowerCase().contains("cinema") ||
+							description.toLowerCase().contains("movies") || description.toLowerCase().contains("theatre") ) {
+						System.out.println("Category:Entertainment:: " + description);
+						entertainment.increment();
+					} else {
+					
+						//utilities - FIRSTSERVICE, PAYPAL
+						if ( description.toLowerCase().contains("rent") || description.toLowerCase().contains("paypal") ||
+								description.toLowerCase().contains("firstservice") ) {
+							System.out.println("Category:Utilities:: " + description);
+							utilities.increment();
+						} else {
+						
+							//misc
+							if ( description.toLowerCase().contains("atm") ) {
+								System.out.println("Category:Miscellaneous:: " + description);
+								misc.increment();
+							} else {
+								System.out.println("Not categorized:: " + description);
+								none.increment();
+							}
+						}
+					}
+				}
+			}
 		}
 	}
 	
 	public void displayCounts() {
 		
+		int total = food.getValue() + travel.getValue() + shopping.getValue() + 
+				entertainment.getValue() + utilities.getValue() + misc.getValue();
+		System.out.println("Total Categorized Transactions: " + total);
+		System.out.println("Total Uncategorized Transactions: " + none.getValue() + "\n");
+		
+		System.out.println("***** Categorizing Transactions *****");
 		System.out.println("Food: " + food.getValue());
 		System.out.println("Travel: " + travel.getValue());
 		System.out.println("Shopping: " + shopping.getValue());
 		System.out.println("Entertainment: " + entertainment.getValue());
 		System.out.println("Utilities: " + utilities.getValue());
 		System.out.println("Miscellaneous: " + misc.getValue());	
+	}
+	
+	public static boolean checkUpperCase(String string) {
+		boolean upperCase = string.equals(string.toUpperCase());
+		return upperCase;
 	}
 }
